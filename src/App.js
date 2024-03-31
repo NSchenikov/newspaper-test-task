ef0230ec2ebc6b9fc2e871cf0ae8c4b0fdd360d8
Nikita-Schenikov <nikita-shenikov@mail.ru>
2024-03-31 19:26:55 +0300

    get posts response from API

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
-
+import { useState, useEffect } from 'react';
+import { getPosts } from './API';
 import './App.css';
 
 function App() {
+
+  let [errorMessage, setErrorMessage] = useState("");
+
+  const errorProcessing = (error) => {
+    // setIsLoading(false);
+    // setPosts([]);
+    setErrorMessage("Something get wrong. Please, try again later");
+    if (error.response && error.response.status === 401) {
+      console.error("Ошибка аутентификации: Некорректная авторизационная строка");
+    } else {
+      console.error("Ошибка при получении данных: ", error);
+    }
+  }
+
+  useEffect(() => {
+    getPosts()
+    .catch(error => {
+      errorProcessing(error);
+    })
+  }, [])
+
   return (
     <div className="App">
+      {errorMessage ? <div>${errorMessage}</div> : ""}
     </div>
   );
 }
